0bcc429e1a3e3fe8a8c1480dca68cf47f91758c7
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-26 19:30:51 +0530

    MTTest Update

---
+++ MTTest.java
@@ -0,0 +1,76 @@
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MTTest
+{
+	public int minValue=(int)Double.POSITIVE_INFINITY;
+	public int minIndex=0;
+	public final int testSize=minValue/4096;
+	public int noOfThreads;
+	public int[] array;
+	public Minimum[] mins;
+	public Thread[] threads; 
+	public MTTest(int n) throws InterruptedException, ExecutionException
+	{
+		noOfThreads=n;
+		array=new int[testSize];
+	    for (int i=0;i<array.length;i++)
+	      array[i] = testSize-i;
+	    ExecutorService pool = Executors.newFixedThreadPool(noOfThreads);
+	    Future<int[]>[] future=new Future[noOfThreads];
+	    for(int i=0;i<noOfThreads;i++)
+	    {
+	    	Callable<int[]> callable=new Minimum(i,noOfThreads);
+	    	future[i]=pool.submit(callable);
+	    }
+	    for(int i=0;i<noOfThreads;i++)
+	    	if(future[i].get()[0]<minValue)
+	    	{
+	    		minValue=future[i].get()[0];
+	    		minIndex=future[i].get()[1];
+	    	}
+	}
+	public class Minimum implements Callable<int[]>
+	{
+		int factor1;
+		int factor2;
+		int minVal=(int)Double.POSITIVE_INFINITY;
+		int minInd;
+		public Minimum()
+		{
+			
+		}
+		public Minimum(int f1,int f2)
+		{
+			factor1=f1;
+			factor2=f2;
+		}
+		public void run()
+		{
+			for(int i=factor1;i<MTTest.this.testSize;i+=factor2)
+				if(MTTest.this.array[i]<minVal)
+				{
+					minVal=MTTest.this.array[i];
+					minInd=i;
+				}
+		}
+		public int[] call() throws Exception
+		{
+			int[] returnArray=new int[2];
+			run();
+			System.out.println(minVal+","+minInd);
+			returnArray[0]=minVal;
+			returnArray[1]=minInd;
+			return returnArray;
+		}
+	}
+	public static void main(String[] args) throws InterruptedException, ExecutionException
+	{
+		MTTest test=new MTTest(4);
+		System.out.println("Final Minimum:"+test.minValue+","+test.minIndex);
+	}
+}
